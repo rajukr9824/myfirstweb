2e713e071bebb5455fd0e65ef23e565a4d4bc65d
rajukr9824 <130588040+rajukr9824@users.noreply.github.com>
2024-07-10 19:12:20 +0530

    Update Textform.js
    corrected some features

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -46,16 +46,16 @@ export default function Textform(props) {
 <div class="mb-3" >
   <textarea class="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'black':'white', color:props.mode==='dark'?'white':'black'} } id="mybox" rows="8"></textarea>
 </div>
-<button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-<button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
-<button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-<button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-<button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove ExtraSpaces</button>
+<button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+<button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
+<button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear Text</button>
+<button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCopy}>Copy Text</button>
+<button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleExtraSpaces}>Remove ExtraSpaces</button>
     </div>
  <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
    <h2>Text summary</h2>
-   <p>{text.split(" ").length} words {text.length} characters</p>
-   <p>{0.008 * text.split(" ").length} Minutes read</p>
+   <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words {text.length} characters</p>
+    <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
    <h2>Preview</h2>
    <p>{text.length>0?text:"Enter text above to preview"}</p>
  </div>
